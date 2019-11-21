648890839a55699acafe2aafa7a438af5288a04c
Franklin Farias <franklin.farias@hospitaldocoracao-al.com.br>
2019-11-21 15:34:52 -0300

    ARIA-70 Correção da API de setores.
    Alterações realizadas:
    
    - Inserido campo sigla na classe de dominio Setor.

--- server/grails-app/domain/br/com/hospitaldocoracaoal/aria/Setor.groovy
+++ server/grails-app/domain/br/com/hospitaldocoracaoal/aria/Setor.groovy
@@ -7,10 +7,12 @@ class Setor {
 
     String descricao
     SetorWpd setorWpd
+    String sigla
     TipoSetor tipoSetor
 
     static constraints = {
         descricao nullable: false, blank: false, unique: true
+        sigla nullable: false, blank: false, unique: true
         setorWpd nullable: false, unique: true
     }
 
