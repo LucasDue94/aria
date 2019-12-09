3687dfff663f679cc586418c2be806c744cc89ce
Franklin Farias <franklin.farias@hospitaldocoracao-al.com.br>
2019-12-09 08:36:26 -0300

    ARIA-7 - Alterações iniciais da API do perfil epidemiologico filtro CID ambulatorial;

--- server/queries/atendimento.sql
+++ server/queries/atendimento.sql
@@ -1,15 +1,15 @@
-CREATE SERVER ambcor_dev FOREIGN DATA WRAPPER postgres_fdw OPTIONS (host 'dev.hcal.lan', dbname 'ambcor', port '5432');
+CREATE SERVER ambcor_dev FOREIGN DATA WRAPPER postgres_fdw OPTIONS (host 'dev.hcal.lan', dbname 'hamb_dev', port '5432');
+drop server ambcor_dev cascade;
 
 CREATE USER MAPPING FOR aria SERVER ambcor_dev OPTIONS (user 'aria', password 'aria@123-hcor');
 
-CREATE FOREIGN TABLE  atendimento (
-    id                      bigint not null,
-    version                 bigint,
-    cid_id                  varchar(255)                                           not null,
-    paciente_id             varchar(255)                                           not null,
-    usuario_id              bigint                                                 not null,
-    registro_atendimento_id varchar(255)                                           not null,
-    data_atendimento        timestamp                                              not null,
-    conteudo                text
+CREATE FOREIGN TABLE atendimento (
+    id bigint not null,
+    version bigint,
+    cid_id varchar(255) not null,
+    usuario_id bigint not null,
+    registro_atendimento_id varchar(255) not null,
+    data_atendimento timestamp not null,
+    conteudo text
     )
-SERVER ambcor_dev OPTIONS (table_name 'atendimento');
+    SERVER ambcor_dev OPTIONS (table_name 'atendimento');
