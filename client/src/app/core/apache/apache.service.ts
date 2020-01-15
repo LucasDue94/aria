ea234fffd1eecbbaa0f18b4b392a34e9c4efb376
Nywton Barros <nywton.barros@gmail.com>
2020-01-15 10:42:40 -0300

    Refactor service Apache

--- client/src/app/core/apache/apache.service.ts
+++ client/src/app/core/apache/apache.service.ts
@@ -1,9 +1,8 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../environments/environment";
 import {Observable, Subject, of} from "rxjs";
 import {map, catchError} from "rxjs/operators";
-import {HeadersHelper} from "../headersHelper";
 import {Apache} from "./apache";
 import {Admissao} from "../setor/admissao";
 import {RegistroAtendimento} from "../registroAtendimento/registroAtendimento";
@@ -11,25 +10,16 @@ import {RegistroAtendimentoLeito} from "../registroAtendimentoLeitos/registroAte
 
 
 @Injectable()
-export class ApacheService extends HeadersHelper {
+export class ApacheService {
 
   private baseUrl = environment.apiUrl;
 
-  getDefaultHttpOptions() {
-    return new HttpHeaders({
-      "Cache-Control": "no-cache",
-      "Content-Type": "application/json",
-      "X-Auth-Token": localStorage.getItem('token')
-    })
-  }
-
   constructor(private http: HttpClient) {
-    super()
   }
 
   list(setorId: number, termo?: string, offset?: any, max?: any): Observable<Admissao[]> {
     let subject = new Subject<RegistroAtendimentoLeito[]>();
-    this.http.get<RegistroAtendimentoLeito[]>(this.baseUrl + `setor/admissoes?` + 'setorId=' + setorId + '&termo=' + termo + '&offset=' + offset + '&max=' + max, {headers: this.getDefaultHttpOptions()})
+    this.http.get<RegistroAtendimentoLeito[]>(this.baseUrl + `setor/admissoes?` + 'setorId=' + setorId + '&termo=' + termo + '&offset=' + offset + '&max=' + max)
       .pipe(
         catchError(error => of({error})
         )).subscribe((json: Admissao[]) => {
@@ -40,7 +30,7 @@ export class ApacheService extends HeadersHelper {
 
   report(dataInicio?: string, dataFim?: string, setorId?: number, offset?: any, max?: any): Observable<Apache[]> {
     let subject = new Subject<any[]>();
-    this.http.get<any[]>(this.baseUrl + `apache/relatorio?` + 'dataInicio=' + dataInicio + '&dataFim=' + dataFim + '&setorId='+ setorId +'&offset=' + offset + '&max=' + max, {headers: this.getDefaultHttpOptions()})
+    this.http.get<any[]>(this.baseUrl + `apache/relatorio?` + 'dataInicio=' + dataInicio + '&dataFim=' + dataFim + '&setorId='+ setorId +'&offset=' + offset + '&max=' + max)
       .pipe(
         catchError(error => of({error})
         )).subscribe((json: any) => {
@@ -63,7 +53,7 @@ export class ApacheService extends HeadersHelper {
 
   get(id: string): Observable<any> {
     let subject = new Subject<RegistroAtendimento>();
-    this.http.get(this.baseUrl + `registroAtendimento/` + id, {headers: this.getDefaultHttpOptions()})
+    this.http.get(this.baseUrl + `registroAtendimento/` + id)
       .pipe(
         catchError(error => of({error})
         )).subscribe((json: any) => {
@@ -79,7 +69,7 @@ export class ApacheService extends HeadersHelper {
 
   search(setorId: number, termo?: string, offset?: any, max?: any): Observable<Admissao[]> {
     let subject = new Subject<Admissao[]>();
-    this.http.get(this.baseUrl + `setor/admissoes?` + 'setorId=' + setorId + '&termo=' + termo + '&offset=' + offset + '&max=' + max, {headers: this.getDefaultHttpOptions()})
+    this.http.get(this.baseUrl + `setor/admissoes?` + 'setorId=' + setorId + '&termo=' + termo + '&offset=' + offset + '&max=' + max)
       .pipe(
         catchError(error => of({error})
         )).subscribe((json: Admissao[]) => {
@@ -92,7 +82,6 @@ export class ApacheService extends HeadersHelper {
     let subject = new Subject<Apache>();
     if (apache.id) {
       this.http.put<Apache>(this.baseUrl + `apache/` + apache.id, apache, {
-        headers: this.getDefaultHttpOptions(),
         responseType: 'json'
       }).pipe(
         catchError(error => of({error}))
@@ -101,7 +90,6 @@ export class ApacheService extends HeadersHelper {
       });
     } else {
       this.http.post<Apache>(this.baseUrl + `apache/`, apache, {
-        headers: this.getDefaultHttpOptions(),
         responseType: 'json'
       }).pipe(
         catchError(error => of({error}))
@@ -115,7 +103,6 @@ export class ApacheService extends HeadersHelper {
 
   destroy(apache: Apache): Observable<Object> {
     return this.http.delete(this.baseUrl + `apache/` + apache.id, {
-      headers: this.getDefaultHttpOptions(),
       observe: 'response'
     });
   }
