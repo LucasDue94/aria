532c45ec9d6354f4df6914041d4be611188ad58e
Nywton Barros <nywton.barros@gmail.com>
2020-01-15 10:37:55 -0300

    Adicionado interceptor http

---
+++ client/src/app/interceptor/request-interceptor.ts
@@ -0,0 +1,41 @@
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpHeaders,
+  HttpInterceptor,
+  HttpRequest
+} from "@angular/common/http";
+import {Observable, throwError} from "rxjs";
+import {catchError} from "rxjs/operators";
+import {Router} from "@angular/router";
+
+
+export class RequestInterceptor implements HttpInterceptor {
+
+  constructor(private router?: Router) {
+
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let cloneReq = req;
+    if(!req.url.includes('login')) {
+      cloneReq = req.clone({
+        headers: new HttpHeaders({
+          "Cache-Control": "no-cache",
+          "Content-Type": "application/json",
+          "X-Auth-Token": localStorage.getItem('token')
+        })
+      })
+    }
+    return next.handle(cloneReq).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if(error.status == 401) {
+          localStorage.clear();
+          this.router.navigate(['/']);
+        }
+        return throwError(error);
+      })
+    );
+  }
+}
