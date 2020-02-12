78c6edfd8f8d890d394fe6d6d99fd88e51427fb3
Lucas Dué <lucas.due@hospitaldocoracao-al.com.br>
2020-02-12 17:57:07 -0300

    - Criação da classe NAS

--- client/src/app/paciente-info/paciente-info.component.ts
+++ client/src/app/paciente-info/paciente-info.component.ts
@@ -28,5 +28,4 @@ export class PacienteInfoComponent implements OnInit {
     return Math.floor(Math.ceil(Math.abs(nascimento.getTime() - (new Date()).getTime()) / (1000 * 3600 * 24)) / 365.25);
   }
 
-
 }
