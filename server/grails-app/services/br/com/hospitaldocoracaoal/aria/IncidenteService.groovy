ea489b369220d8c49a1aabfbf44797160755f7c1
Nywton Barros <nywton.barros@gmail.com>
2020-02-13 17:18:48 -0300

    Incidentes form [progress]

--- server/grails-app/services/br/com/hospitaldocoracaoal/aria/IncidenteService.groovy
+++ server/grails-app/services/br/com/hospitaldocoracaoal/aria/IncidenteService.groovy
@@ -1,18 +1,65 @@
 package br.com.hospitaldocoracaoal.aria
 
+
+import br.com.hospitaldocoracaoal.integracao.RegistroAtendimento
+import br.com.hospitaldocoracaoal.integracao.RegistroAtendimentoLeito
+import br.com.hospitaldocoracaoal.integracao.SetorWpd
 import grails.gorm.services.Service
 
 @Service(Incidente)
-interface IncidenteService {
+abstract class IncidenteService {
 
-    Incidente get(Serializable id)
+    abstract Incidente get(Serializable id)
 
-    List<Incidente> list(Map args)
+    abstract List<Incidente> list(Map args)
 
-    Long count()
+    abstract Long count()
 
-    void delete(Serializable id)
+    abstract void delete(Serializable id)
 
-    Incidente save(Incidente incidente)
+    Incidente save(Incidente incidente, String pacienteId) {
+        List<RegistroAtendimento> registros = (List<RegistroAtendimento>) RegistroAtendimento.createCriteria().list(sort: 'dataEntrada') {
+            paciente {
+                eq 'id', pacienteId
+            }
 
+            le 'dataEntrada', incidente.dataHora
+            or {
+                ge 'dataAlta', incidente.dataHora
+                isNull 'dataAlta'
+            }
+        }
+
+        if (registros == null && registros.isEmpty()) {
+            // TODO: ERROR
+        }
+
+        RegistroAtendimento registroAtendimento = registros.last()
+        SetorWpd setorWpd = null
+
+        switch (registroAtendimento.tipo) {
+            case 'I':
+                RegistroAtendimentoLeito registroAtendimentoLeito = registroAtendimento.registroAtendimentoLeitos
+                        .sort { ral1, ral2 -> ral1.dataEntrada <=> ral2.dataEntrada }
+                        .find {  it.dataEntrada <= incidente.dataHora }
+                setorWpd = registroAtendimentoLeito.leito.setor
+                break;
+            case 'A':
+            case 'E':
+            case 'U':
+                setorWpd = registroAtendimento.setor
+                break;
+            default:
+                setorWpd = null // TODO: ERROR
+        }
+
+        incidente.registroAtendimento = registroAtendimento
+        incidente.setor = Setor.findBySetorWpd setorWpd
+
+        if (!incidente.validate()) {
+            // TODO: ERROR
+        }
+
+        incidente.save(flush: true)
+    }
 }
