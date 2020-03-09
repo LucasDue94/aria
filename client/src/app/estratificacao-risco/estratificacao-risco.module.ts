4b7fcd9f75ac87c2f80d4ec43ab133dcba379582
Franklin Farias <franklin.farias@hospitaldocoracao-al.com.br>
2020-03-09 18:01:49 -0300

    Finalizacao parte Riscos

--- client/src/app/estratificacao-risco/estratificacao-risco.module.ts
+++ client/src/app/estratificacao-risco/estratificacao-risco.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EstratificacaoRiscoFormComponent } from './form/estratificacao-risco-form.component';
 import {PacienteInfoModule} from "../paciente-info/paciente-info.module";
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 
 
 
 @NgModule({
   declarations: [EstratificacaoRiscoFormComponent],
-  imports: [
-    CommonModule,
-    PacienteInfoModule
-  ],
+    imports: [
+        CommonModule,
+        PacienteInfoModule,
+        FontAwesomeModule
+    ],
   exports: [
     EstratificacaoRiscoFormComponent
   ]
