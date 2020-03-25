09cb870261ffc73ae9f475e92aaf1b369ced9096
Franklin Farias <franklin.farias@hospitaldocoracao-al.com.br>
2020-03-25 18:02:00 -0300

    Otimizacao estratificacao-risco;

--- client/src/app/risco/estratificacao/grupo-fator-risco/grupo-fator-risco.component.ts
+++ client/src/app/risco/estratificacao/grupo-fator-risco/grupo-fator-risco.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {AbstractControl, FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {EstratificacaoRisco} from "../../../core/estratificacaoRisco/estratificacaoRisco";
 
 @Component({
