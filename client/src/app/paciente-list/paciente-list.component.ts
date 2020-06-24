81e8d5767a11409640902dd6e257bd2248c25d85
franklin.farias <franklin.farias@hospitaldocoracao-al.com.br>
2020-06-24 17:52:33 -0300

    Lista de pacientes - progresso...

--- client/src/app/paciente-list/paciente-list.component.ts
+++ client/src/app/paciente-list/paciente-list.component.ts
@@ -1,10 +1,12 @@
 import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 import {SetorService} from "../core/setor/setor.service";
 import {PacienteService} from "../core/paciente/paciente.service";
-import {Paciente} from "../core/paciente/paciente";
 import {Setor} from "../core/setor/setor";
 import {faMinus, faPlus} from "@fortawesome/free-solid-svg-icons";
 import {TitleService} from "../core/title/title.service";
+import {RegistroLeitoService} from "../core/registroLeito/registro-leito.service";
+import {RegistroLeito} from "../core/registroLeito/registroLeito";
+import {FilterService} from "../core/filter/filter.service";
 
 @Component({
   selector: 'paciente-list',
@@ -14,14 +16,32 @@ import {TitleService} from "../core/title/title.service";
 
 export class PacienteListComponent implements OnInit {
 
+
   @ViewChild('collapse', {static: false}) collapse: ElementRef;
-  setores: Setor[] = [];
+  offset = 0;
+  max = 30;
+  setorId;
   faPlus = faPlus;
   faMin = faMinus;
-  listLoading;
-  pacientes: Paciente[] = [];
+  listLoading = true;
+  showListScrollSpinner = false;
+  spinner;
+  params = {
+    termo: '',
+    inicio: '',
+    fim: '',
+    setorId: '',
+    offset: this.offset,
+    max: this.max
+  };
+  setores: Setor[] = [];
+  registroLeitos: RegistroLeito[] = [];
+  searchEmpty = true;
 
-  constructor(private titleService: TitleService, private setorService: SetorService, private pacienteService: PacienteService, private render: Renderer2) {
+  constructor(private titleService: TitleService, private setorService: SetorService,
+              private pacienteService: PacienteService, private render: Renderer2,
+              private registroLeitoService: RegistroLeitoService, private filterService: FilterService) {
+    this.search = this.search.bind(this);
   }
 
   ngOnInit(): void {
@@ -29,16 +49,47 @@ export class PacienteListComponent implements OnInit {
     this.setorService.list().subscribe(setor => {
       this.setores = setor;
     });
-    this.pacienteService.list().subscribe(paciente => {
-      this.pacientes = paciente;
-    })
+    this.filterService.receive().subscribe(this.search);
   }
 
   scrollDown() {
-
+    this.showListScrollSpinner = true;
+    this.offset += 30;
+    this.getRegistros();
   }
 
-  open(id?: any) {
+  getStatusSearch(statusSearch) {
+    this.searchEmpty = statusSearch;
+  }
+
+  setFilterParams(params) {
+    this.params.setorId = '';
+    this.registroLeitos = [];
+    this.params.termo = params.busca;
+    this.searchEmpty = params.busca === '' || params.busca === null;
+  }
+
+  search(params) {
+    this.offset = 0;
+    this.listLoading = true;
+    this.setFilterParams(params);
+    if (params) { this.offset += 30; this.getRegistros(); }
+    this.listLoading = params.busca === '' || params.busca === null;
+  }
+
+  getRegistros() {
+    this.registroLeitoService.list(this.params).subscribe(registro => {
+      this.registroLeitos = registro;
+      this.showListScrollSpinner = false;
+      this.listLoading = false;
+    });
+  }
+
+  open(id: any) {
+    this.params.setorId = id;
+    this.listLoading = true;
+    this.getRegistros();
+    this.render.addClass(this.collapse.nativeElement, 'container-scroll-none');
     this.collapse.nativeElement.childNodes.forEach(node => {
       if (node.id === id) {
         if (node.lastChild.classList.contains('collapse-none')) {
@@ -46,15 +97,26 @@ export class PacienteListComponent implements OnInit {
           this.render.addClass(node.childNodes[0].lastChild, 'collapse-none');
         }
       }
+      if (node.lastChild != null) {
+        if (node.lastChild.classList.value === 'collapse-content collapse-none') {
+          this.render.addClass(node, 'collapse-none');
+        }
+      }
     });
   }
 
   close(id: any) {
+    this.render.removeClass(this.collapse.nativeElement, 'container-scroll-none');
     this.collapse.nativeElement.childNodes.forEach(node => {
       if (node.id === id) {
         if (node.lastChild.classList.contains('collapse-content')) {
           this.render.addClass(node.lastChild, 'collapse-none');
           this.render.removeClass(node.childNodes[0].lastChild, 'collapse-none');
+        }
+      }
+      if (node.lastChild != null) {
+        if (node.lastChild.classList.value === 'collapse-content collapse-none') {
+          this.render.removeClass(node, 'collapse-none');
         }
       }
     });
