e89133575edbd8d03d2e8790b4adae757647bed5
franklin.farias <franklin.farias@hospitaldocoracao-al.com.br>
2020-07-29 09:46:36 -0300

    COMPONENT DIAGNOSTIC

--- client/src/app/admissao/diagnostico/status/diagnostico-status.component.ts
+++ client/src/app/admissao/diagnostico/status/diagnostico-status.component.ts
@@ -1,48 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-import {Cid} from '../../../core/cid/cid';
-import {faMinusCircle, faClipboardCheck, faExclamationCircle} from '@fortawesome/free-solid-svg-icons';
+import {AfterViewInit, Component, OnInit, Renderer2} from '@angular/core';
+import {faMinusCircle} from '@fortawesome/free-solid-svg-icons';
+import {EnumStatusCid} from '../../../core/cid/enumStatusCid';
 
 @Component({
   selector: 'app-diagnostico-status',
   templateUrl: './diagnostico-status.component.html',
   styleUrls: ['./diagnostico-status.component.scss']
 })
-export class DiagnosticoStatusComponent implements OnInit {
+export class DiagnosticoStatusComponent implements OnInit, AfterViewInit {
 
-  cidsSelected: Cid[] = [];
   faMinusCircle = faMinusCircle;
-  faClipboardCheck = faClipboardCheck;
   diagnosticStatus = [
     {id: 1, status: 'Suspeita'},
     {id: 2, status: 'Confirmado'},
     {id: 3, status: 'Descartado'}
   ];
 
-  constructor() { }
+  constructor(private render: Renderer2) {
+  }
 
   ngOnInit() {
-    console.log(this.diagnosticStatus);
   }
 
-  setStatusCid(status?, cid?, targetStatus?) {
-   /* let diagnostic = null;
-    this.diagnosticStatus.forEach(situation => {
-      if (status === situation.status) {
-        const statusResult = this.diagnosticStatus.filter(cidStatus => cidStatus.status === status).reduce(item => item);
-        diagnostic = Object.assign(statusResult, cid);
-      }
-    });
-    this.setCidStatusColors(diagnostic, targetStatus);*/
+  ngAfterViewInit(): void {
+
   }
 
-
-  removeCid(diagnostic: Cid) {
-  /*  this.cidsSelected = this.cidsSelected.filter(c => c.id !== diagnostic.id);
-    if (this.cidsSelected.length === 0) {
-      this.currentStep = 0;
-      this.fastSearchVisibility = true;
+  setStatusColors(event) {
+    const buttonStatus = event.target;
+    switch (buttonStatus.innerText) {
+      case EnumStatusCid.SUSPEITA:
+        this.render.addClass(buttonStatus, 'active-status-suspicious');
+        break;
+      case EnumStatusCid.CONFIRMADO:
+        this.render.addClass(buttonStatus, 'active-status-confirm');
+        break;
+      case EnumStatusCid.DESCARTADO:
+        this.render.addClass(buttonStatus, 'active-status-descarded');
     }
-    this.listAtendimentoCid = this.listAtendimentoCid.filter(atendimentoCid => atendimentoCid.cid !== diagnostic);
-    this.diagnostic.emit(this.listAtendimentoCid);*/
   }
 }
