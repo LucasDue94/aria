f6ca3bc7d93bac1a9ac4bbb5452e8a8d4a21c5a9
Nywton Barros <nywton.barros@gmail.com>
2020-08-07 16:51:19 -0300

    Progress...

--- client/src/app/painel-cirurgico/painel-cirurgico/painel-cirurgico.component.ts
+++ client/src/app/painel-cirurgico/painel-cirurgico/painel-cirurgico.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TitleService} from "../../core/title/title.service";
 
 @Component({
   selector: 'app-painel-cirurgico',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelCirurgicoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titleService: TitleService) { }
 
   ngOnInit() {
+    this.titleService.send('Painel Cirúrgico');
   }
 
 }
