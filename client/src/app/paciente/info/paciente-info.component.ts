b72c0d4810db7056447eeb8ba61a90717482423c
Lucas Dué <lucas.due@hospitaldocoracao-al.com.br>
2020-08-14 16:05:17 -0300

    - Ajustes no componente paciente info - Plano terapêutico em andamento

--- client/src/app/paciente/info/paciente-info.component.ts
+++ client/src/app/paciente/info/paciente-info.component.ts
@@ -2,18 +2,27 @@ import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 import {faWindowMinimize} from '@fortawesome/free-solid-svg-icons/faWindowMinimize';
 import {Paciente} from '../../core/paciente/paciente';
 import {faPlus} from '@fortawesome/free-solid-svg-icons/faPlus';
+import {Leito} from '../../core/leito/leito';
 
 @Component({
   selector: 'paciente-info',
   templateUrl: './paciente-info.component.html',
   styleUrls: ['./paciente-info.component.scss']
 })
+/**
+ * @property backgroundColor recebe uma string com a cor personalizada do container.
+ * @property shadow aplica uma sombra ao container principal do componente.
+ * @property useMinimizeButton define se vai existir ou não um botão para minimizar o componente.
+ * @property useBigName destaca o nome do paciente com uma fonte maior.
+ */
 export class PacienteInfoComponent implements OnChanges {
   @Input() paciente: Paciente;
+  @Input() leito: Leito;
   @Input() extras = [];
   @Input() backgroundColor: string;
   @Input() shadow: string;
   @Input() useMinimizeButton = true;
+  @Input() useBigName = false;
   windowMinimizeIcon = faWindowMinimize;
   faPlus = faPlus;
   minimize = false;
